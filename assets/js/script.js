38b5593ae6673417c3bcc5e4269baba19d09eb01
PGCranford <PGCranford@gmail.com>
2022-05-21 19:13:54 -0400

    start of creating buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
+
 
 var taskFormHandler = function (event) {
     event.preventDefault();
@@ -35,23 +37,34 @@ var taskFormHandler = function (event) {
 
 
 var createTaskEl = function (taskDataObj) {
+
+    var creatTaskActions = function (taskId) {
+        var actionContainerEl = document.createElement("div");
+        actionContainerEl.className = "task-actions";
+    };
     // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
+    // add task id as a custom attribute
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
+
     //create div to hold task info and add to list item
 
-    var taskInfoEl = document.createElement("div")
+    var taskInfoEl = document.createElement("div");
 
     taskInfoEl.className = "task-info";
 
-    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskDataObj.name + "</h3><span class = 'task-type'>" + taskDataObj.type + "</span>"
+    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskDataObj.name + "</h3><span class = 'task-type'>" + taskDataObj.type + "</span>";
 
-    listItemEl.appendChild(taskInfoEl)
+    listItemEl.appendChild(taskInfoEl);
 
     // add entire list item to list 
 
     tasksToDoEl.appendChild(listItemEl);
+
+    // increase task counter for next unique id
+    taskIdCounter++;
 
 };
 
